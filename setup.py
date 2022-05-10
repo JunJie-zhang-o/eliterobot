7b3647e129d8f7b6eccd447d11c3703723129641
zhangjunjie <1157860961@qq.com>
2022-05-10 14:57:06 +0800

    updata

--- setup.py
+++ setup.py
@@ -2,28 +2,46 @@
 Author: Elite_zhangjunjie
 CreateDate: 
 LastEditors: Elite_zhangjunjie
-LastEditTime: 2022-05-09 21:08:35
+LastEditTime: 2022-05-10 14:17:21
 Description: 
 '''
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
 setuptools.setup(
     # 项目名称
-    name = "elibot",
+    name = "elirobot",
     # 版本号
-    version = "",
+    version = "0.0.1",
     # 作者
-    author = "",
+    author = "Elite",
     # 邮箱
     author_email = "",
+    # url地址
+    url= "https://github.com/JunJie-zhang-o/eliterobot.git",
     # 描述
-    description="",
+    description = "a SDK library for Elite EC Series Robot ",
     # 长描述
-    long_description="",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     # 安装依赖
-    install_requires = [],
-    # 自动发现根目录中所有的子包
-    packages=setuptools.find_packages(),
+    install_requires = ["loguru>=0.6.0"],
+    # 自动发现根目录中所有的子包,find_packages()只能打包python包，python包中只有.py而不包括这个文件夹下 其他的数据文件。并且根目录下的.py文件不会被打包进去
+    packages = setuptools.find_packages(
+        # where=".",
+        # include=["elite*"],
+        # exclude=["elite.EliteJson_3_0_2",
+        #          "elite.ElitePoseCali",
+        #          "elite.pose.py"]
+        ),
+
     # 附加信息
-    classifiers=[]
+    classifiers = [],
+    # python版本要求
+    python_requites=">3.5",
+    # 关键字
+    keywords = ["elite","elibot","eliterobot","robot","Robot","robotSDK"]
 )
